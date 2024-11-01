a26fb931ef581772fc3fd62cdae3f9cd8c00edab
emeren <tomasz.szenborn@gmail.com>
2024-11-02 00:49:22 +0100

    add hop and toggleterm plugins

---
+++ nvim/lua/plugins/hop.lua
@@ -0,0 +1,26 @@
+return {
+  'phaazon/hop.nvim',
+  branch = 'v2', -- optional but strongly recommended
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    local hop = require 'hop'
+    local directions = require('hop.hint').HintDirection
+    require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
+    vim.keymap.set('', 'f', function()
+      hop.hint_words { direction = directions.AFTER_CURSOR, current_line_only = true }
+    end, { remap = true })
+    vim.keymap.set('', 'F', function()
+      hop.hint_words { direction = directions.BEFORE_CURSOR, current_line_only = true }
+    end, { remap = true })
+    vim.keymap.set('', 't', function()
+      hop.hint_words { current_line_only = false, hint_offset = -1 }
+    end, { remap = true })
+    vim.keymap.set('', 'T', function()
+      hop.hint_char1 { current_line_only = false, hint_offset = 1 }
+    end, { remap = true })
+
+    vim.cmd 'hi HopNextKey guifg=#52AD70'
+    vim.cmd 'hi HopNextKey1 guifg=#52AD70'
+    vim.cmd 'hi HopNextKey2 guifg=#52AD70'
+  end,
+}
---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<c-\>]],
+    },
+    config = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+      end
+
+      require('toggleterm').setup {
+        open_mapping = [[<leader>j]],
+        vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()',
+      }
+
+      local vertical = Terminal:new {
+        direction = 'vertical',
+        on_open = function(self)
+          self:resize(80)
+        end,
+      }
+      local horizontal = Terminal:new {
+        direction = 'horizontal',
+        on_open = function(self)
+          self:resize(12)
+        end,
+      }
+      vim.keymap.set({ 't', 'n' }, '<leader>j', function()
+        horizontal:toggle()
+      end)
+
+      vim.keymap.set({ 't', 'n' }, '<leader>]', function()
+        vertical:toggle()
+      end)
+    end,
+  },
+}
